1bb1075f4d6d6d41d549340a91857035c81f1c6a
miilanv993 <milan.z.velickovic993@hotmail.com>
2020-03-10 22:29:26 +0100

    Implemented Card and CardList components

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Milan',
+        email: 'julianne.OOConner@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Milan',
+        email: 'lucio_hettinger@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Milan',
+        email: 'karley_Dach@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Milan',
+        email: 'milan@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Milan Bauch',
+        username: 'Milan',
+        email: 'milan@april.biz'
+    },
+    {
+        id: 9,
+        name: 'Milan Bauch',
+        username: 'Milan',
+        email: 'milan@april.biz'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Milan',
+        email: 'Rey.Padberg@april.biz'
+    }
+]
