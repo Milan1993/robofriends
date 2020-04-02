82131d75f8e13475efbdbe167a49ebe448e34110
Milan Velickovic <milan.velickovic@pearson.com>
2020-04-02 09:27:22 +0200

    Added Redux

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChamge: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  }
+}
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  render() {
+    const { searchField, onSearchChamge, robots, isPending } = this.props;
+
+    const filterRobotos = robots.filter(robots => {
+      return robots.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return isPending ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={onSearchChamge} />
+          <Scroll>
+            <CardList robots={filterRobotos} />
+          </Scroll>
+        </div>
+      )
+}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
